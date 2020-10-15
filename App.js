ae5806ccef337914e5e4644b51ca336e81236bbb
Vinicius-Ricci <viniricci02@gmail.com>
2020-10-14 21:21:22 -0300

    Primeiro commit

---
+++ App.js
@@ -0,0 +1,152 @@
+import { StyleSheet, View, Image, TouchableOpacity,Text } from 'react-native';
+import React,{Component} from 'react';
+
+export default class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+
+      numero: 0,
+      botao : 'Vai',
+      ultimo: null 
+    };
+
+    
+    //Variavel do timer do relogio
+    this.timer = null;
+
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
+
+
+  }
+
+  vai(){
+
+    if(this.timer != null){
+      //Aqui vai parar o timer
+      clearInterval(this.timer); 
+      this.timer = null;
+      this.setState({botao: 'Vai'});
+    }else{
+      //Começa girar o timer
+    this.timer = setInterval(() => {
+
+      this.setState({numero: this.state.numero + 0.1})
+    },100);
+
+    this.setState({botao: 'Parar'});
+    
+    }
+  }
+
+  limpar(){
+
+    if(this.timer != null ){
+        //Aqui vai parar o timer
+        clearInterval(this.timer); 
+        this.timer = null;
+    }
+      this.setState({
+        ultimo: this.state.numero,
+        numero: 0,
+        botao: 'Vai'
+      })
+  }
+
+  render(){
+    return(
+
+      <View style={styles.container}>
+       
+      <Image 
+      source={require('./src/cronometro.png')}
+      style={styles.img}
+      />
+
+      <Text style={styles.timer}> {this.state.numero.toFixed(1)} </Text>  
+
+
+      <View style={styles.btnArea}>
+
+      <TouchableOpacity style={styles.btn} onPress={this.vai}>
+      <Text style={styles.btnTexto}> {this.state.botao} </Text>  
+      </TouchableOpacity>  
+
+
+      <TouchableOpacity style={styles.btn} onPress={this.limpar}>
+      <Text style={styles.btnTexto}> Limpar </Text>  
+      </TouchableOpacity>
+
+      </View>
+
+        <View style={styles.areaUltimo}>
+
+            <Text style={styles.textoCorrida}>
+               {this.state.ultimo > 0 ? 'Ultimo tempo: ' + this.state.ultimo.toFixed(2) + 's' : ''}
+            </Text>
+
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+  container:{
+    flex: 1,
+    backgroundColor: '#fca311',
+    alignItems: 'center',
+    justifyContent: 'center'
+    
+  },
+  
+  img:{
+
+  },
+
+  timer:{
+    marginTop: -160,
+    color: '#FFF',
+    fontSize: 65,
+    fontWeight: 'bold'
+
+  },
+
+  btnArea:{
+    flexDirection: 'row',
+    marginTop: 80,
+    height: 40
+
+  },
+
+  btn:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 9
+
+  },
+
+  btnTexto:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fca311'
+  },
+
+  areaUltimo:{
+    marginTop: 50
+  },
+
+  textoCorrida:{
+    fontSize: 25,
+    fontStyle: 'italic',
+    color: '#FFF'
+  }
+});
